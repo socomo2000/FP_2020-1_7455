b24ef70df89f366f4d2089402c85ec23a316e91c
socomo2000 <54279512+socomo2000@users.noreply.github.com>
2019-11-06 11:17:59 -0600

    Update main.c

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -3,13 +3,13 @@
 int main(int argc, char*argv[])
 {
 float x, y, t;
-x=atof(argv[1]);
-t=x;
-y=atof(argv[2]);
-while(y>1){
-x=x*t;
-y=y-1;
+x=atof(argv[1]);//se convierte a flotante el primer argumento
+t=x;//para poder multiplicar a x por si misma
+y=atof(argv[2]);//se convierte a flotante el segundo argumento
+while(y>1){//hasta que el contador llegue a 1 se detendra el programa
+x=x*t;//para multiplicar el numero por si mismo tantas veces sea necesario
+y=y-1;//contador para que el programa se detenga
 }
-printf("%.2lf\n",x);
+printf("%.2lf\n",x);//el resultado tiene dos decimales
 return 0;
 }
