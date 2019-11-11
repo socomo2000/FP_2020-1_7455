c3876d59ce89a981ad5e420ca82a10deb8cdf777
socomo2000 <54279512+socomo2000@users.noreply.github.com>
2019-11-11 17:48:18 -0600

    Update main.c

--- TAREAS/12/main.c
+++ TAREAS/12/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-int APRETONES(int personas){
-  int res;
+int APRETONES(int personas){//funcion para decir numero de personas y regresar cuantos saludos hubo dentre ellas si no se repitieronm saludos(usando recursividad)
+  int res;//declarar variables
   int z;
-  if(personas==1){
-    return 0;
-  }else{
-    z=APRETONES(personas-1)+(personas-1);
-    return z;
+  if(personas==1){//en caso de que solo sea una perona no habrá saludos, este se usará como nuestro caso base
+    return 0;//ya que no hay saludos, se regresa cero
+  }else{//si es mas de una persona...
+    z=APRETONES(personas-1)+(personas-1);//el resultdo es la suma de todos los naturales que anteceden al numero sin contar al numero
+    return z;//se regresa dicha suma
   }
 }
-int main(int argc, char*argv[]) {
+int main(int argc, char*argv[]) {//
 int a, ans;
-a=atoi(argv[1]);
-ans=APRETONES(a);
-printf("%i\n",ans);
+a=atoi(argv[1]);//se lee y transforma a int el numero ingresado
+ans=APRETONES(a);//se usa la funcion para calcular el numero de apretones
+printf("%i\n",ans);//se muestra el resultado
 return 0;
 }
